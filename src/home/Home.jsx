989553e1610b51bfe7f157b20a362bca9102fada
Michal Kowalczyk <michal98kowalczyk@gmail.com>
2023-04-05 20:19:05 +0200

    routing init

---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        < main >
+            <section>
+                <h1>home</h1>
+            </section>
+        </main >
+    );
+}
+export default Main;
